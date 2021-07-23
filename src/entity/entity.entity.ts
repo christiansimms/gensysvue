36acb6727d1eff2ff0de14c5c80eb3762cbebf59
Christian Simms <csimms@botonomy.com>
2021-07-23 18:06:39 -0400

    Start saving a table.

---
+++ src/entity/entity.entity.ts
@@ -0,0 +1,25 @@
+import { Entity as EntityCl, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@EntityCl()
+export class Entity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  parent_id: number;
+
+  @Column()
+  type: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  fields: string; // TODO: Should be JSONB?.
+
+  @Column()
+  last_updated_by: number;
+
+  @Column()
+  last_updated_date: string; // TODO: should be timestamp
+}
--- src/entity/entity.module.ts
+++ src/entity/entity.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EntityService } from './entity/entity.service';
 import { EntityController } from './entity/entity.controller';
+import { Entity } from './entity.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Entity])],
   providers: [EntityService],
-  controllers: [EntityController]
+  controllers: [EntityController],
 })
 export class EntityModule {}
