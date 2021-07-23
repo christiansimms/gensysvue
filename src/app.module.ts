aadbf6a859ccbc326342cc3dea5c656281b8e1de
Christian Simms <csimms@botonomy.com>
2021-07-22 21:36:24 -0400

    start entity module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EntityModule } from './entity/entity.module';
 
 @Module({
-  imports: [],
+  imports: [EntityModule],
   controllers: [AppController],
   providers: [AppService],
 })
