4b5412ba0736d0ee866f7c2d77187b7de8262a00
Christian Simms <csimms@botonomy.com>
2021-09-14 20:42:01 -0400

    Start over.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import {ref, Ref} from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
+const someObject: Ref<{ banana: string }> = ref({ banana: 'they are good' });
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
